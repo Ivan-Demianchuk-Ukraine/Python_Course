ed94c38784b8736f576e03e5b55bd43deffa1abf
Ivan <donework2000@gmail.com>
2022-10-27 15:47:54 +0300

    added HomeWork-3

---
+++ HomeWork-3.py
@@ -0,0 +1,36 @@
+# Task №1
+a = int(input('Enter number A:'))
+b = int(input('Enter number B:'))
+c = int(input('Enter number C:'))
+if a > 10 and b > 10 and c > 10 and a % 3 == 0 and b % 3 == 0:
+    print('yes')
+else:
+    print('no')
+
+# Task №2
+a = int(input('Enter number A:'))
+b = int(input('Enter number B:'))
+c = int(input('Enter number C:'))
+if a > b and a > c:
+    Max = a
+    print(Max)
+elif b > a and b > c:
+    Max = b
+    print(Max)
+elif c > a and c > b:
+    Max = c
+    print(Max)
+else:
+    print('Max number is absent')
+
+# Task №3
+a = int(input('Enter the number A:'))
+b = int(input('Enter the number B:'))
+c = []
+if a < b:
+    for i in range(a, b + 1, 1):
+        if i % 2 == 0:
+            c.append(i)
+elif a >= b:
+    print('"B" number - too small')
+print('Your Result: ', sum(c))
