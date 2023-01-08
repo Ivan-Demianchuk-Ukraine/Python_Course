9ad10c3b7a6563a7a3c307edfeb53c427d816e3e
Ivan <donework2000@gmail.com>
2023-01-09 00:31:02 +0300

    changed names for variables on more understandable

--- all_home_works/home_work_19.py
+++ all_home_works/home_work_19.py
@@ -1,47 +1,29 @@
 class AttributePrinterMixin:
     def __str__(self):
-
-        test = self.__dict__
-
+        all_attributes_dict = self.__dict__
         inherit_three = A.__mro__
-        massive_with_classes = []
+        list_with_class_names = []
         for i in inherit_three:
             if i.__name__ != 'object':
-                massive_with_classes.append(i.__name__)
-
-        value_dict = {}
-        # result_dict = {
-        #     self.__class__.__name__: value_dict
-        # }
-
-        count = 0
-        for i, value in test.items():
-# -------------------------for A privated class-------------------------------------
-            if i[0] == '_' and i[1:4] == self.__class__.__name__+'__':
-                i = i.replace('_', '', 1)
-                i = i.replace(f'{self.__class__.__name__}__', '', 1)
-# -------------------------for A privated class-------------------------------------
-# -------------------------for inhereted privated class-------------------------------------
-            for _ in massive_with_classes:
-                len_class_count = len(_)
-                if i[0] == '_' and i[1:len_class_count + 1] == _:
-                    i = i.replace('_', '', 1)
-                    i = i.replace(f'{_}__', '', 1)
-#-------------------------for inhereted privated class-------------------------------------
-# -------------------------for ALL class for all protected attributes-------------------------------------
-            if i[0] == '_' and i[1] != '_':
-                i = i.replace(i[0], '', 1)
-# -------------------------for ALL class for all protected attributes-------------------------------------
-            value_dict[i] = value
-        print(self.__class__.__name__, ':', sep='')
-
-        amount_items = len(value_dict)
-        for _ in value_dict:
-            print('    ', _, ': ', value_dict[_], sep='')
-            # if count < amount_items:
-                # count += 1
-        print('}')
-        return ''
+                list_with_class_names.append(i.__name__)
+        all_attributes_updated = {}
+        for key, value in all_attributes_dict.items():
+            self_class_length = len(self.__class__.__name__)
+            if key[0] == '_' and key[1:self_class_length+3] == self.__class__.__name__+'__':
+                key = key.replace('_', '', 1)
+                key = key.replace(f'{self.__class__.__name__}__', '', 1)
+            for name in list_with_class_names:
+                len_class_count = len(name)
+                if key[0] == '_' and key[1:len_class_count + 1] == name:
+                    key = key.replace('_', '', 1)
+                    key = key.replace(f'{name}__', '', 1)
+            if key[0] == '_' and key[1] != '_':
+                key = key.replace(key[0], '', 1)
+            all_attributes_updated[key] = value
+        print(self.__class__.__name__, ':', ' {', sep='')
+        for _ in all_attributes_updated:
+            print('    ', _, ': ', all_attributes_updated[_], sep='')
+        return '}'
 
 
 class B:
@@ -55,7 +37,7 @@ class C:
     def __init__(self):
         self.s4 = 54
         self._s5 = [54]
-        self.__s6 = (54)
+        self.__s6 = 54
 
 
 class A(AttributePrinterMixin, B, C):
