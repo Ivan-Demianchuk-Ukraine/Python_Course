1fc7bc13769c04cc7f57ba70880b91e7496a9799
Ivan <donework2000@gmail.com>
2023-01-15 15:12:43 +0300

    added fix to reported problem.

--- all_home_works/Context_Manager_Timer_HM_21.py
+++ all_home_works/Context_Manager_Timer_HM_21.py
@@ -7,6 +7,7 @@ class Timer:
         self.start_time = time.time()
 
     def __enter__(self):
+        self.start_time = time.time()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
@@ -24,15 +25,11 @@ with Timer() as t:
     time.sleep(1)
     for i in range(0, 10000000):
         pass
-print(t.elapsed_time())  # ~1.2 second
+print(t.elapsed_time())  # ~1.4 second
 
 with t:
     time.sleep(2)
-print(t.elapsed_time())  # ~3.2 seconds
-
-with t:
-    time.sleep(2)
-print(t.elapsed_time())  # ~5.2 seconds
+print(t.elapsed_time())  # ~2 seconds
 
 with Timer() as t2:
     time.sleep(1)
@@ -41,3 +38,13 @@ t2.reset()
 with t2:
     time.sleep(2)
 print(t2.elapsed_time())  # ~2 seconds
+
+with Timer() as t:
+    time.sleep(1)
+print(t.elapsed_time())  # ~1 as expected
+
+time.sleep(3)
+
+with t:
+    time.sleep(2)
+print(t.elapsed_time())  # expected ~2, but got ~6
