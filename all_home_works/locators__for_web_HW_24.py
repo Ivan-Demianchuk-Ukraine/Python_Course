6f3fb091635db8e10f8b9257a0c74c5692568180
Ivan <donework2000@gmail.com>
2023-01-30 00:52:35 +0300

    added 12 locators for future tests.

---
+++ all_home_works/locators__for_web_HW_24.py
@@ -0,0 +1,21 @@
+# https://www.spacex.com/
+# Написать 5 XPath и 5 CSS локаторов для элементов страницы.
+# Это могут быть одни и те же элементы или разные.
+# Добавить скриншот с обведенными элементами, для которых вы пишете локаторы.
+# Для каждого элемента использовать разные подходы в написании локаторов (т.е. не должно быть что все 5 локаторов записаны по id).
+
+
+locator_1_by_id = "hamburger"
+locator_2_by_xpath = '//*[@id="menu-navigation"]/ul/li[13]/a'
+locator_3_by_class = "Heading u-h6"
+locator_4_by_xpath = '//*[@id="menu-kids"]/div/div[3]/a"'
+locator_5_by_xpath =  '//*[@id="shopify-section-collection-template"]/section/div/div/div/div/ul/li[1]/div/div/div/span/a'
+locator_6_by_xpath = '//*[@id="product_form_4790840361039"]/button'
+locator_7_by_id = 'country'
+#there I want to select Ukraine from the drop-down but I can't
+locator_8_by_class = 'ShippingEstimator__Zip Form__Input'
+#there I should enter some int value
+locator_9_by_class = 'ShippingEstimator__Submit Button Button--primary'
+locator_10_by_class = 'ProductItem__Image Image--fadeIn lazyautosizes Image--lazyLoaded'
+locator_11_by_class = 'QuantitySelector__Button Link Link--secondary'
+locator_12_by_class = '//*[@id="product_form_6703932538959"]/button/span'
