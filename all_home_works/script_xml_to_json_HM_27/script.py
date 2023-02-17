06953772044bddc314eb5ea120eef16f587c7c36
Ivan <donework2000@gmail.com>
2023-02-17 23:32:21 +0300

    added final solution

--- all_home_works/script_xml_to_json_HM_27/script.py
+++ all_home_works/script_xml_to_json_HM_27/script.py
@@ -1,25 +1,27 @@
-
-
 def converter_xml_to_json(xml_file):
     import xml.etree.ElementTree as ET
     import json
     three = ET.parse(f'{xml_file}')
     root = three.getroot()
     json_info = {}
-    count = 0
+    page_count = 0
     for page in root:
         page = page.attrib['name']
         json_info[f'{page}'] = {}
-        for element in root[count].iter('element'):
+        i = 0
+        for element in root[page_count].iter('element'):
             json_info[f'{page}'][element.attrib['name']] = {}
-            for locator in root[count].iter('locator'): #тут надо чтобы не in root[count] было, а глубже типо root[count][element]
-                json_info[f'{page}'][element.attrib['name']][locator.attrib['platform']] = [locator.attrib['locator_type']] + [locator.text]
 
-        count += 1
+            for locator in root[page_count][i].iter('locator'):
+                json_info[f'{page}'][element.attrib['name']][locator.attrib['platform']]\
+                    = [locator.attrib['locator_type']] + [locator.text]
+            i = i + 1
+        page_count += 1
+
     json_info = json.dumps(json_info, indent=4)
     with open('json_new_file.json', 'w') as f:
         f.writelines(json_info)
     return json_info
 
 
-print(converter_xml_to_json('xml_file.xml'))
+converter_xml_to_json('xml_file.xml')
